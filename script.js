4577783e5974358a276134fc7419a9963dd61c2a
Claudio <claudiom.pst@gmail.com>
2023-08-02 22:51:14 -0300

    catch method

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
 .then(resposta => resposta.json())
-.then(r => console.log(r))
+.then(r => {
+    if(r.erro){
+        throw Error('Esse Cep é inexistente!')
+    } else {
+        console.log(r)
+    }
+})
+.catch(erro => console.log(erro))
 
 console.log(consultaCEP)
