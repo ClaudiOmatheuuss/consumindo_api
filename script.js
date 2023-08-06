3db969efc7c5942a8f593ebf1c7efec905d0179a
Claudio <claudiom.pst@gmail.com>
2023-08-06 11:09:05 -0300

    doing dynamic query at cep input

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-async function buscaEndereco(){
+async function buscaEndereco(cep){
     try {
-        var consultaCEP = await fetch('https://viacep.com.br/ws/01001250/json/')
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
         var consultaCEPConvertido = await consultaCEP.json()
         if (consultaCEPConvertido.erro){
             throw Error('CEP não existente!')
         }
         console.log(consultaCEPConvertido)
+        return consultaCEPConvertido
     } catch(erro) {
         console.log(erro)
     }
 }
 
-buscaEndereco()
+var cep = document.getElementById('cep')
+cep.addEventListener('focusout' , () => buscaEndereco(cep.value))
